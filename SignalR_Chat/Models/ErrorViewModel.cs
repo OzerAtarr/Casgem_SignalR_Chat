0b83c52f53029462971db013d5f1b171dbd71b04
90536 <90536@DESKTOP-FCHRJ42>
2023-08-01 22:18:02 +0300

    Add project files.

---
+++ SignalR_Chat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_Chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SignalR_Chat/Program.cs
@@ -0,0 +1,32 @@
+using SignalR_Chat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR(); // signal ar 
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+   
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chatHub"); //signalar
+
+app.Run();
